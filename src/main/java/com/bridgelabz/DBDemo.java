30887bba1e479a832e7fea3c6a29fbe281325428
we <reddypallyradhika@gmail.com>
2022-10-31 13:42:47 -0700

    Started Section 3: JDBC: EmployeePayroll Service:UC1

--- src/main/java/com/bridgelabz/DBDemo.java
+++ src/main/java/com/bridgelabz/DBDemo.java
@@ -4,7 +4,21 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.Enumeration;
-
+/*
+Ability to create a payroll service
+database and have java program
+connect to database
+- Use the payroll_service database created in MySQL
+- Download and register the jdbc driver jar to your Build
+Path in Eclipse
+- Alternatively use Maven or Gradle to add jdbc driver to
+your project
+- Check if the Driver Class is available for the java
+program
+- Check all the drivers registered with the jdbc driver
+- Check if the database connection to payroll_service
+mysql DB is established
+ */
 public class DBDemo {
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
@@ -41,7 +55,7 @@ public class DBDemo {
     private static void listDrivers(){
         Enumeration<Driver> driverList = DriverManager.getDrivers();
         while (driverList.hasMoreElements()){
-            Driver driverClass = (Driver) driverList.nextElement();
+            Driver driverClass = driverList.nextElement();
             System.out.println(" "+driverClass.getClass().getName());
         }
 
